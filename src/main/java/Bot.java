7f4af21c64733c494cac55ec8a1824245efd851a
ReGius-igmt <56514865+ReGius-igmt@users.noreply.github.com>
2022-04-28 01:02:04 +0300

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -17,8 +17,22 @@ public class Bot extends TelegramLongPollingBot {
 
    public Random rnd = new Random();
    public int a = rnd.nextInt(100);
-    ArrayList<String> arrayList = new ArrayList<>();
+   /**
+      Название переменных должно быть осмысленным
+      * chats
+      * chatIds
+      Для хранения уникальных значений больше подойдет Set
+      Мог бы и Long хранить а не String
+      
+      Хранение id чатов, хотя должен бы id пользователей
+   **/
+   ArrayList<String> arrayList = new ArrayList<>();
 
+    /**
+      Из того что видно в обработке сообщения 
+      Увеличивается и уменьшается в зависимости от кол-ва чатов
+      Назначение не ясно
+    **/
     byte check = 0;
 
     @SneakyThrows
@@ -64,6 +78,17 @@ public class Bot extends TelegramLongPollingBot {
                }
             }
         }
+       /**
+         i = 0
+         0 < 1 true
+         i++
+         1 < 1 false
+         
+         Циклы отрабатывают 1-н раз и сравнивают значение 1-го элемента списка
+         с id пользователя отправившнего сообшение
+         При этом оба цикла выполняют одно и то же действие
+         Не важно чему равно check, в результате будет выполнено Test(message)
+       **/
         for (int i = 0; i < 1; i++) {
             if (arrayList.get(i).equals(message.getChatId().toString()) && message.hasText() && check == 1) {
                 Test(message);
@@ -86,7 +111,18 @@ public class Bot extends TelegramLongPollingBot {
                     execute(SendMessage.builder().chatId(message.getChatId().toString()).text("Больше").build());
                 } else if (i1 > a) {
                     execute(SendMessage.builder().chatId(message.getChatId().toString()).text("Меньше").build());
+                   /**
+                     Хотя a это примитив int и li теоретически будет неявно преобразован в приметив
+                     Лучше использовать equals или явно указать li как приметив int используя Integer.parseInt
+                     Оценка 2 за сравнение элемента ссылочного типа
+                   **/
                 } else if (a == i1) {
+                   /**
+                     a - случайное число
+                     li - чило от пользователя
+                     Попадание сюда гаранитирует что a == li
+                     В чем смысл перевода a в строку и сравнения с message.getText()?
+                   **/
                     String str = Integer.toString(a);
                     if (message.hasText()) {
                         if (message.getText().equals(str)) {
@@ -94,6 +130,9 @@ public class Bot extends TelegramLongPollingBot {
                             a = rnd.nextInt(100);
                         }
                     }
+                   /**
+                   Невозможная ситуация, обрабатывает 4-й случай из 3 возможных (число либо больше, либо меньше, либо равно, никак иначе)
+                   **/
                 }else{ execute(SendMessage.builder().chatId(message.getChatId().toString()).text("Бро зачем?").build());}
             }
 }
